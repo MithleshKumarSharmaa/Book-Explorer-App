d6af543ce59497fa7f178315b2292bf33bf85a95
Mithlesh Sharma <mithleshkumarsharmaa@gmail.com>
2026-01-08 06:09:57 +0530

    Implemention with Two logic and check comment code and same folder both logic

--- lib/data/data_sources/book_local_data_source.dart
+++ lib/data/data_sources/book_local_data_source.dart
@@ -1,6 +1,8 @@
 import 'package:hive/hive.dart';
 import 'package:take_home/data/local_models/book_local_model.dart';
 
+/// This below code local db create by api response id
+/*
 class BookLocalDataSource {
   Box<BookLocalModel> get box => Hive.box<BookLocalModel>('books');
 
@@ -35,9 +37,40 @@ class BookLocalDataSource {
     await box.delete(id);
   }
 
-  // if (!updatedBook.isFavorite && !updatedBook.isRead) {
-  //   await box.delete(id);
-  // } else {
-  //   await box.put(id, updatedBook);
-  // }
+} */
+
+/// This below code local db create by api response isbn
+class BookLocalDataSource {
+  Box<BookLocalModel> get box => Hive.box<BookLocalModel>('books');
+
+  Future<void> saveBook(BookLocalModel book) async {
+    await box.put(book.isbn, book);
+  }
+
+  Future<void> updateBookStatus(
+    String isbn, {
+    bool? isFavorite,
+    bool? isRead,
+  }) async {
+    final book = box.get(isbn);
+    if (book != null) {
+      final updatedBook = book.copyWith(
+        isFavorite: isFavorite ?? book.isFavorite,
+        isRead: isRead ?? book.isRead,
+      );
+      await box.put(isbn, updatedBook);
+    }
+  }
+
+  List<BookLocalModel> getAllBooks() {
+    return box.values.toList();
+  }
+
+  BookLocalModel? getBook(String isbn) {
+    return box.get(isbn);
+  }
+
+  Future<void> deleteBook(String isbn) async {
+    await box.delete(isbn);
+  }
 }
--- lib/presentation/bloc/book_detail/book_detail_bloc.dart
+++ lib/presentation/bloc/book_detail/book_detail_bloc.dart
@@ -28,4 +28,3 @@ class BookDetailBloc extends Bloc<BookDetailEvent, BookDetailState> {
     });
   }
 }
-
